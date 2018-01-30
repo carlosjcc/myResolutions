03be7a368544763c57063fb9d69ef2abe9486654
carlosjcc <carloscorralesch@gmail.com>
2018-01-30 13:42:14 +1100

    inserts data into database

--- App.jsx
+++ App.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-class App extends React.Component {
+Resolutions = new Mongo.Collection("resolutions");
+
+export default class App extends React.Component {
+
+  addResolution(event) {
+    event.preventDefault();
+    var text = this.refs.resolution.value.trim();
+
+    Resolutions.insert({
+      text: text,
+      complete: false,
+      createdAt: new Date()
+    });
+
+    this.refs.resolution.value = "";
+  }
+
   render() {
     return (
       <div>
-        <h1> Hello World </h1>
+        <h1> My Resolutions </h1>
+        <form className="new-resolution" onSubmit={this.addResolution.bind(this)}>
+          <input
+                type="text"
+                ref="resolution"
+                placeholder="Finish React Meteor Series"/>
+        </form>
       </div>
     )
   }
-}
-
-if (Meteor.isClient) {
-  Meteor.startup(() => {
-    render(<App />, document.getElementById("render-target"))
-  });
 }
