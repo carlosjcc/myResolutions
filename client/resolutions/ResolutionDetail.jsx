f6b2d8321f00b46ab2f55c203e60ba139ba8ed38
carlosjcc <carloscorralesch@gmail.com>
2018-01-31 23:44:46 +1100

    adds parameter based routing

---
+++ client/resolutions/ResolutionDetail.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import TrackerReact from 'meteor/ultimatejs:tracker-react';
+
+export default class ResolutionDetail extends TrackerReact(Component) {
+
+  constructor() {
+    super();
+    this.state = {
+      subscription: {
+        resolutions: Meteor.subscribe("userResolutions")
+      }
+    }
+  }
+
+  componentWillUnmount() {
+    this.state.subscription.resolutions.stop();
+  }
+
+  resolution() {
+    // find returns a cursos - fetch returns the object
+    //return Resolutions.find({_id: this.props.id}).fetch();
+    return Resolutions.findOne(this.props.id);
+  }
+  
+  render() {
+
+    let res = this.resolution();
+    if (!res) {
+      return(<div>Loading...</div>);
+    }
+
+    return (
+      <div>
+        <h1>{res.text}</h1>        
+      </div>
+    )
+  }
+}
